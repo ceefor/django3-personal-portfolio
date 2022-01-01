c2e72e211a1412a9a34d6783e2af37c8d1659e48
Evilbyte <ceefor@gmail.com>
2022-01-01 01:28:16 +0000

    ls

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j*fb*pvbzfl_gfvtfk_e-7m((l$whp7@3_3&5)%7tfd4jl4esa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evilbyte.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
